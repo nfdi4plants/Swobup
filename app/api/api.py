62a9e6d8315805b9acc8b8b10a4d777c7c34155f
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-07-18 18:19:57 +0200

    renamed api methods

--- app/api/api.py
+++ app/api/api.py
@@ -12,14 +12,14 @@ from app.api.endpoints import templates
 #from endpoints import ping
 
 api_router = APIRouter()
-api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
+# api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
 # api_router.include_router(ping.router, prefix="/ping2")
 
 api_router.include_router(health.router, prefix="/health", tags=["Status"])
-api_router.include_router(ontology.router, prefix="/ontology", tags=["Custom"])
-api_router.include_router(add_ext_ontology.router, prefix="/add_extern", tags=["Custom"])
-api_router.include_router(clean_database.router, prefix="/setup", tags=["setup"])
-api_router.include_router(add_ext_ontology.router, prefix="/setup", tags=["setup"])
-api_router.include_router(initiate_database.router, prefix="/setup", tags=["setup"])
+api_router.include_router(ontology.router, prefix="/ontology", tags=["Webhooks"])
+api_router.include_router(add_ext_ontology.router, prefix="/add_extern", tags=["Ontologies"])
+api_router.include_router(clean_database.router, prefix="/setup", tags=["Setup"])
+api_router.include_router(add_ext_ontology.router, prefix="/setup", tags=["Setup"])
+api_router.include_router(initiate_database.router, prefix="/setup", tags=["Setup"])
 
-api_router.include_router(templates.router, prefix="/template", tags=["Custom"])
+api_router.include_router(templates.router, prefix="/template", tags=["Templates"])
