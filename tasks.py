ea8c328f650c6ef1675431330b2e5acdfe77b296
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-07-18 10:58:29 +0200

    added template model

--- tasks.py
+++ tasks.py
@@ -14,6 +14,7 @@ imports = ['app.tasks.process_ontology',
 app = Celery('tasks',
              backend=os.environ.get("CELERY_BACKEND"),
              broker=os.environ.get("CELERY_BROKER"),
+             
              )
 
 print("celery started...")
