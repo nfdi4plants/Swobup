c7e6de04c3909a03eed333ba3afbe86d469345e6
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-07-19 02:48:26 +0200

    added description to api calls

--- app/api/endpoints/initiate_database.py
+++ app/api/endpoints/initiate_database.py
@@ -2,13 +2,11 @@ from fastapi import APIRouter, Body, Depends, HTTPException, status, FastAPI
 from fastapi.responses import PlainTextResponse, JSONResponse, HTMLResponse
 from app.neo4j.neo4jConnection import Neo4jConnection
 
-
-
 router = APIRouter()
+
 
 @router.put("/initiate")
 async def initiate_db():
-
     conn = Neo4jConnection(uri="bolt://localhost:7687",
                            user="neo4j",
                            pwd="test")
