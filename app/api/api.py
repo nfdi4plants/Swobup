8283bc6eee3cef7f7419a79ae1bf3859eecef4f6
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-07-19 03:10:40 +0200

    restructured database api and added api prefix

--- app/api/api.py
+++ app/api/api.py
@@ -8,18 +8,20 @@ from app.api.endpoints import add_ext_ontology
 from app.api.endpoints import clean_database
 from app.api.endpoints import initiate_database
 from app.api.endpoints import templates
+from app.api.endpoints import database
 
 #from endpoints import ping
 
-api_router = APIRouter()
+api_router = APIRouter(prefix="/api/v2")
 # api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
 # api_router.include_router(ping.router, prefix="/ping2")
 
 api_router.include_router(health.router, prefix="/health", tags=["Status"])
 api_router.include_router(ontology.router, prefix="/ontology", tags=["Webhooks"])
 api_router.include_router(add_ext_ontology.router, prefix="/add_extern", tags=["Ontologies"])
-api_router.include_router(clean_database.router, prefix="/setup", tags=["Database"])
+api_router.include_router(clean_database.router, prefix="/setup", tags=["Database"], deprecated=True)
 api_router.include_router(add_ext_ontology.router, prefix="/setup", tags=["Setup"], deprecated=True)
-api_router.include_router(initiate_database.router, prefix="/setup", tags=["Database"])
+api_router.include_router(initiate_database.router, prefix="/setup", tags=["Database"], deprecated=True)
 
 api_router.include_router(templates.router, prefix="/template", tags=["Templates"])
+api_router.include_router(database.router, prefix="/database", tags=["Database"])
---
+++ app/api/endpoints/database.py
@@ -0,0 +1,55 @@
+import obonet
+from io import StringIO
+import sys
+import pandas as pd
+
+from celery.result import AsyncResult
+from celery import chain
+
+from fastapi import APIRouter, Body, Depends, HTTPException, status
+from fastapi.responses import PlainTextResponse, JSONResponse, HTMLResponse
+from app.github.webhook_payload import PushWebhookPayload
+
+from app.tasks.process_ontology import ontology_task
+from app.custom.custom_payload import CustomPayload
+from app.custom.models.add_ontology import AddOntologyPayload
+from app.custom.models.delete_ontology import DeleteOntologyPayload, DeleteOntologyResponse
+
+from app.github.downloader import GitHubDownloader
+from app.helpers.obo_parser import OBO_Parser
+
+from app.tasks.add_external_ontologies import add_extern_task
+from app.tasks.delete_ontologies import delete_ontology_task
+from app.tasks.add_to_database import write_to_db
+
+from app.tasks.clear_database_task import clear_database_task
+from app.neo4j.neo4jConnection import Neo4jConnection
+
+from resource import *
+
+router = APIRouter()
+
+
+@router.delete("/clear", response_model=DeleteOntologyResponse, status_code=status.HTTP_201_CREATED,
+               summary="Clear database")
+async def clear_database():
+    print("deleting database")
+
+    result = clear_database_task.delay()
+
+    print("results2", result.get())
+
+    return DeleteOntologyResponse(
+        deleted=result.get()
+    )
+
+
+@router.put("/init", summary="Initiate database and setting constraints")
+async def initiate_db():
+    conn = Neo4jConnection(uri="bolt://localhost:7687",
+                           user="neo4j",
+                           pwd="test")
+
+    conn.set_constraints()
+
+    return JSONResponse(status_code=201, content="bla")
