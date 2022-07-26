db080199b9e02dda68e4ff72857486e73da0bad4
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-07-26 02:06:49 +0200

    api cleaning

--- app/tasks/ontology_tasks.py
+++ app/tasks/ontology_tasks.py
@@ -160,3 +160,20 @@ def ontology_build_from_scratch():
         #                        pwd="test")
         # # conn.update_template2(template.dict())
         # conn.add_ontologies()
+
+    @app.task
+    def delete_ontology_task(payload):
+
+        conn = Neo4jConnection(uri="bolt://127.0.0.1:7687",
+                               user="neo4j",
+                               pwd="test")
+
+        print("payload", payload)
+
+        if payload.get("url"):
+            print("url is available")
+
+        if payload.get("ontology"):
+            print("ontologies were given")
+            for ontology_name in payload.get("ontology"):
+                conn.delete_ontology(ontology_name)
