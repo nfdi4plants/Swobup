8c026719e81eb5f0db970e7cab1f97ea80c934bf
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-07-28 18:02:46 +0200

    changed branch to hash

--- app/api/endpoints/webhooks.py
+++ app/api/endpoints/webhooks.py
@@ -18,7 +18,6 @@ from app.tasks.ontology_tasks import ontology_build_from_scratch
 from app.github.downloader import GitHubDownloader
 from app.helpers.obo_parser import OBO_Parser
 
-
 from app.helpers.models.ontology.term import Term
 from app.helpers.models.ontology.ontology import Ontology
 from app.helpers.models.ontology.relationships import Relationships
@@ -50,12 +49,7 @@ router = APIRouter()
 @router.post("/ontology", summary="Ontology Webhook", status_code=status.HTTP_204_NO_CONTENT,
              response_class=Response)
 async def ontology(request: Request, payload: PushWebhookPayload):
-
     print("sending to celery...")
-
-    # ret = chain(ontology_task.s(payload.commits), ontology_task.s(payload.commits)).apply_async()
-
-    # bla = {"bla":"blu"}
 
     payload_dictionary = payload
 
@@ -63,22 +57,11 @@ async def ontology(request: Request, payload: PushWebhookPayload):
 
     print("body", body)
 
-    # print("pay", payload_dictionary)
-
     print("payload", payload.dict())
-
-    # conn = Neo4jConnection(uri="bolt://localhost:7687",
-    #                        user="neo4j",
-    #                        pwd="test")
-    #
-    # term_list = conn.list_terms_of_ontology("nfdi4pso")
-
-    # print("terms", term_list)
-
-    # ret = chain(update_ontology_task.s(payload.commits), ontology_task.s(payload.commits)).apply_async()
 
     repository_name = payload.repository.full_name
     branch = payload.ref.split("/")[-1]
+    branch = payload.after
     hash_id = payload.after
     commits = payload.commits.pop()
 
@@ -111,13 +94,9 @@ async def ontology(request: Request, payload: PushWebhookPayload):
 @router.post("/template", summary="Template Webhook", status_code=status.HTTP_204_NO_CONTENT,
              response_class=Response)
 async def template(request: Request, payload: PushWebhookPayload):
-
     body = await request.body()
 
     print("body", body)
-
-    # print("pay", payload_dictionary)
-
     print("payload", payload.dict())
     repository_name = payload.repository.full_name
     branch = payload.ref.split("/")[-1]
@@ -148,15 +127,7 @@ async def template(request: Request, payload: PushWebhookPayload):
     print("updates", update_urls)
 
     for url in update_urls:
-        #chain(add_ontology_task.s(url), update_ontologies.s()).apply_async()
+        # chain(add_ontology_task.s(url), update_ontologies.s()).apply_async()
         add_template_custom.delay(url)
 
-
-
-
     return Response(status_code=status.HTTP_204_NO_CONTENT)
-
-
-
-
-
