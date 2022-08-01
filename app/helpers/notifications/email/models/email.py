55800ad42089a18eb184070a4392133792328fd9
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-08-01 18:21:39 +0200

    mail + tests

---
+++ app/helpers/notifications/email/models/email.py
@@ -0,0 +1,14 @@
+from typing import Optional, List, Dict
+
+from pydantic import BaseModel
+
+
+class Services(BaseModel):
+    neo4j: str
+    swate: str
+    rabbitmq: str
+
+
+class Health(BaseModel):
+    services: Services
+    status: str
