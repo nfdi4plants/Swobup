b3b43900ad615093e244f86cf9d2d5f8d809016b
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-08-01 18:21:39 +0200

    mail + tests

--- app/helpers/swate_api.py
+++ app/helpers/swate_api.py
@@ -1,15 +1,19 @@
+import os
+
 import requests
 
 
 class SwateAPI:
-    def __init__(self, backend_url):
-        self.backend_url = backend_url
-        self.headers = {"Content-Type":"application/octet-stream"}
+    def __init__(self):
+        # self.backend_url = backend_url
+        self.backend_url = os.getenv("SWATE_API", "https://swate.nfdi4plants.org")
+
+        self.headers = {"Content-Type": "application/octet-stream"}
 
     def convert_xslx(self, file):
         url = self.backend_url + "/api/IISADotNetCommonAPIv1/toSwateTemplateJson"
 
-        response = requests.post(url, data= file, headers=self.headers)
+        response = requests.post(url, data=file, headers=self.headers)
 
         print("url", url)
         print("response", response)
