c69f281cdfb383a61630f1b0bf80b12d483b0ebd
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-08-03 02:37:42 +0200

    added simple status html page

---
+++ app/html/static/api.js
@@ -0,0 +1,58 @@
+const healthStatus = async () => {
+  const response = await fetch('api/v2/status/health');
+  const responseJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+  console.log(responseJson)
+
+    if (responseJson.status == "OK") {
+      document.getElementById("status").innerHTML = "Online";
+      document.getElementById('status').classList.remove('is-danger');
+      document.getElementById('status').classList.add('is-success');
+  }
+    else {
+      document.getElementById("status").innerHTML = "Offline";
+      document.getElementById('status').classList.remove('is-success');
+      document.getElementById('status').classList.add('is-danger');
+
+    }
+
+    if (responseJson.services.neo4j == "connected") {
+      document.getElementById("database_status").innerHTML = "Connected";
+      document.getElementById('database_status').classList.remove('is-danger');
+      document.getElementById('database_status').classList.add('is-success');
+  }
+    else {
+      document.getElementById("database_status").innerHTML = "Disconnected";
+      document.getElementById('database_status').classList.remove('is-success');
+      document.getElementById('database_status').classList.add('is-danger');
+
+    }
+
+
+  document.getElementById("broker_status").innerHTML = responseJson.services.rabbitmq;
+          document.getElementById('broker_status').classList.remove('is-success');
+      document.getElementById('broker_status').classList.add('is-warning');
+  document.getElementById("swate_status").innerHTML = responseJson.services.swate;
+        document.getElementById('swate_status').classList.remove('is-success');
+      document.getElementById('swate_status').classList.add('is-warning');
+}
+
+
+const StatusInformation = async () => {
+  const response = await fetch('api/v2/status/info');
+  const responseJson = await response.json(); //extract JSON from the http response
+  // do something with myJson
+  console.log(responseJson)
+  document.getElementById("number_terms").innerHTML = responseJson.number_terms;
+  document.getElementById("number_ontologies").innerHTML = responseJson.number_ontologies;
+  document.getElementById("number_relationships").innerHTML = responseJson.number_relationships;
+  document.getElementById("db_url").innerHTML = responseJson.db_url;
+}
+
+
+window.onload = function() {
+  healthStatus();
+};
+
+setInterval( healthStatus, 1000);
+setInterval( StatusInformation, 1000);
