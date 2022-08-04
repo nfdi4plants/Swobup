af3c91c3982705b50409c674aa125a140b48656b
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-08-04 17:40:45 +0200

    repaired webhooks

--- app/api/middlewares/github_authentication.py
+++ app/api/middlewares/github_authentication.py
@@ -21,20 +21,11 @@ def generate_hash_signature(
 
 async def github_authentication(request: Request, x_hub_signature_256: str = Header(None)):
     payload = await request.body()
-
-    print("pp", payload)
-
-    # print("payload", json.loads(payload))
     secret = os.environ.get("GITHUB_SECRET").encode("utf-8")
-    # secret = os.environ.get("GITHUB_SECRET").encode()
-    # secret = bytes(os.environ.get("GITHUB_SECRET"), "utf-8")
-    print("secret", secret)
     signature = generate_hash_signature(secret, payload)
     print("sig       ", signature)
     print("sig header", x_hub_signature_256)
-    # if x_hub_signature_256 != f"sha256={signature}":
-    print("--", f"sha256={signature}")
     if x_hub_signature_256 != f"sha256={signature}":
         raise HTTPException(status.HTTP_401_UNAUTHORIZED,
                             detail="Not Authorized")
-    return {}
+    return
