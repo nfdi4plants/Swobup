3b37c52f812db13bdd4fac742fe339ef7645799b
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-08-26 16:24:25 +0200

    added send testmail functionality

--- app/helpers/notifications/email/mail_notifier.py
+++ app/helpers/notifications/email/mail_notifier.py
@@ -10,26 +10,143 @@ from datetime import datetime
 
 import os
 
+
 class MailNotifier:
-    def __init__(self, str_to):
-        self.strCc = os.getenv.get("NOTIFIER_ADDITIONAL_RECEIVER")
-        self.port = os.getenv.get("NOTIFIER_PORT")
-        self.strFrom = os.getenv.get("NOTIFIER_SENDER")
-        self.password = os.getenv.get("NOTIFIER_PASSWORD")
-        self.username = os.getenv.get("NOTIFIER_USERNAME")
-        self.server = os.getenv.get("NOTIFIER_SERVER")
-        self.method = os.getenv.get("MAIL_METHOD", "smtp")
+    def __init__(self):
+        self.strCc = os.environ.get("NOTIFIER_ADDITIONAL_RECEIVER", None)
+        self.port = os.environ.get("NOTIFIER_PORT")
+        self.strFrom = os.environ.get("NOTIFIER_SENDER")
+        self.password = os.environ.get("NOTIFIER_PASSWORD")
+        self.username = os.environ.get("NOTIFIER_USERNAME")
+        self.server = os.environ.get("NOTIFIER_SERVER")
+        self.method = os.environ.get("MAIL_METHOD", "smtp")
+        self.strTo = "marcel.tschoepe@rz.uni-freiburg.de"
+
+        self.template_path = "app/helpers/notifications/email/templates/html/"
+
+        self.html_message = ""
+        self.text_message = ""
+
+        print(os.getcwd())
+
+        self.html_message = open(self.template_path + "main.html", "r").read()
+
 
         if self.username is None:
             self.username = self.strFrom
 
+    def add_headline(self, color, message, repository_name):
+        headline_file = open(self.template_path + "headline.html", "r").read()
+        headline_file = headline_file.replace('$ALERT_COLOR', color)
+        headline_file = headline_file.replace('$ALERT_MESSAGE', message)
+        headline_file = headline_file.replace('$REPO_NAME', repository_name)
+        self.html_message = self.html_message.replace('$MAIN_ALERT', headline_file)
+
+    def set_line_color(self, hex_value):
+        self.html_message = self.html_message.replace('$LINE_COLOR', hex_value)
+
+    def add_main_information(self):
+
+        github_username = ""
+        commit_user = ""
+        commit_mailaddress = ""
+        commit_message = ""
+        commit_timestamp = ""
+
+        if github_username is "":
+            main_info_file = ""
+        else:
+            main_info_file = open(self.template_path + "main_information.html", "r").read()
+            committer_details_file = main_info_file.replace('$GITHUB_USERNAME', github_username)
+            main_info_file = main_info_file.replace('$COMMIT_NAME', commit_user)
+            main_info_file = main_info_file.replace('$COMMIT_MAIL', commit_mailaddress)
+            main_info_file = main_info_file.replace('$COMMIT_MESSAGE', commit_message)
+            main_info_file = main_info_file.replace('$COMMIT_TIMESTAMP', commit_timestamp)
+
+        self.html_message = self.html_message.replace('$MAIN_INFORMATION', main_info_file)
+
+    def add_messages(self):
+        self.html_message = self.html_message.replace('$MESSAGES', "")
+
+    def add_text(self):
+        body_text = open(self.template_path + "testmail_text.html", "r").read()
+        self.html_message = self.html_message.replace('$TEXT', body_text)
+
+    def build_mail(self):
+
+        print("html is now", self.html_message)
+
+        # Create the root message and fill in the from, to, and subject headers
+        msgRoot = MIMEMultipart('related')
+        msgRoot['Subject'] = "Swobup Commit Report"
+        msgRoot['From'] = self.strFrom
+        msgRoot['To'] = self.strTo
+        msgRoot['Cc'] = self.strCc
+        # msgRoot['Date'] = time.ctime(time.time())
+        msgRoot['Date'] = email.utils.formatdate(localtime=True)
+        msgRoot['Message-ID'] = email.utils.make_msgid()
+
+        msgRoot.preamble = 'This is a multi-part message in MIME format.'
+
+        # Encapsulate the plain and HTML versions of the message body in an
+        # 'alternative' part, so message agents can decide which they want to display.
+        msgAlternative = MIMEMultipart('alternative')
+        msgRoot.attach(msgAlternative)
+
+        msgText = MIMEText('This is the alternative plain text message.')
+
+        # msgText = "test"
+
+        # msgText = MIMEText(msgText)
+        msgAlternative.attach(msgText)
+
+        # We reference the image in the IMG SRC attribute by the ID we give it below
+        msgText = MIMEText(self.html_message, 'html')
+        msgAlternative.attach(msgText)
+
+        # This example assumes the image is in the current directory
+        # fp = open(self.template_path + 'logo-blue-text-lightblue.png', 'rb')
+        fp = open(self.template_path + 'logo-blue-small.png', 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+
+        # Define the image's ID as referenced above
+        msgImage.add_header('Content-ID', '<swobup-logo>')
+        msgRoot.attach(msgImage)
+
+        fp = open(self.template_path + 'twitter-logo.png', 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+
+        # Define the image's ID as referenced above
+        msgImage.add_header('Content-ID', '<twitter-logo>')
+        msgRoot.attach(msgImage)
+
+        fp = open(self.template_path + 'github-logo-white.png', 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+
+        # Define the image's ID as referenced above
+        msgImage.add_header('Content-ID', '<github-logo>')
+        msgRoot.attach(msgImage)
+
+        fp = open(self.template_path + 'dataplant-logo.png', 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+
+        # Define the image's ID as referenced above
+        msgImage.add_header('Content-ID', '<dataplant-logo>')
+        msgRoot.attach(msgImage)
+
+        return msgRoot
 
     def send_mail(self, message):
         context = ssl.create_default_context()
-        # addresses = self.strTo + ", " + self.strCc
-        # addresses = addresses.split()
 
-        rcpt_addresses = self.strCc.split(",") + [self.strTo]
+        if self.strCc is not None:
+            rcpt_addresses = self.strCc.split(",") + [self.strTo]
+        else:
+            rcpt_addresses = [self.strTo]
 
         if self.method == "smtp":
             with smtplib.SMTP_SSL(self.server, self.port, context=context) as server:
@@ -48,6 +165,3 @@ class MailNotifier:
                     self.strFrom, rcpt_addresses, message.as_string()
                 )
                 server.quit()
-
-
-
