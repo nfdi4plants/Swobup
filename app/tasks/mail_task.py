f7ca7fd2d2fd15db5ba0ff5faa25f4a28f148107
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-08-26 16:24:25 +0200

    added send testmail functionality

--- app/tasks/mail_task.py
+++ app/tasks/mail_task.py
@@ -6,6 +6,7 @@ import pandas as pd
 from io import StringIO
 
 import time
+import os
 
 from tasks import app
 from app.github.webhook_payload import PushWebhookPayload
@@ -13,6 +14,8 @@ from app.github.downloader import GitHubDownloader
 from app.helpers.obo_parser import OBO_Parser
 
 from celery.result import AsyncResult
+
+from app.helpers.notifications.email.mail_notifier import MailNotifier
 
 from app.neo4j.neo4jConnection import Neo4jConnection
 
@@ -44,3 +47,24 @@ def show_tasks_results(task_ids: list, task_results: list):
         print("::", current_task.result)
         # print("::", current_task.get())
     print("--", task_results)
+
+
+@app.task
+def send_testmail():
+    mail_method = os.environ.get("NOTIFIER_METHOD")
+
+    mail_notifier = MailNotifier()
+    mail_notifier.add_headline("#ffc000", "Swobup Test Message", "")
+    mail_notifier.set_line_color("#4caed3")
+    mail_notifier.add_main_information()
+    mail_notifier.add_text()
+    mail_notifier.add_messages()
+    mail_body = mail_notifier.build_mail()
+
+    print("sending mail...")
+    mail_notifier.send_mail(mail_body)
+    print("mail sent...")
+
+
+
+
