e27143cd7de0b491887c8816ec5251dc22123438
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-09-08 01:33:17 +0200

    added localstorage

--- app/helpers/storage_backend.py
+++ app/helpers/storage_backend.py
@@ -1,15 +1,42 @@
-from UltraDict import UltraDict
-from typing import Any, Dict, Optional, Union, List
+import json
+import os
 
-class Storage_backend:
+from os import path
+
+from celery.backends.s3 import S3Backend
+
+from tasks import app
+
+
+class LocalStorage:
     def __int__(self):
+        self.path = "data"
+
+    def set(self, **params):
         pass
 
-    def store(self):
-        pass
 
-    def get(self):
-        pass
+class StorageBackend:
+    def __int__(self):
+        self.mode:bool = False
+        self.backend = None
 
-    def delete(self):
-        pass
+        if os.environ.get("storage") == "local":
+            self.backend = LocalStorage()
+            self.mode = False
+        else:
+            self.backend = S3Backend(app=app)
+            self.mode = True
+
+    def store(self, task_id, data):
+        if self.mode:
+            self.backend.set(key=task_id, value=json.dumps(data))
+        else:
+            self.backend.set()
+
+    def get(self, task_id):
+        data = self.backend.get(task_id)
+        return data
+
+    def delete(self, task_id):
+        self.backend.delete(task_id)
