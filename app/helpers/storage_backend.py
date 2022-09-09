f232f309b96d0411d89902f01fda2344d49dd9be
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2022-09-09 14:05:27 +0200

    added local storage backend and new environment variable

--- app/helpers/storage_backend.py
+++ app/helpers/storage_backend.py
@@ -6,37 +6,61 @@ from os import path
 from celery.backends.s3 import S3Backend
 
 from tasks import app
+from pathlib import Path
 
 
 class LocalStorage:
-    def __int__(self):
-        self.path = "data"
+    def __init__(self):
+        self.storage_dir = "localstorage"
+        Path(self.storage_dir).mkdir(exist_ok=True)
+        # if not os.path.exists(self.storage_dir):
+        #     print("creating directory")
+        #     os.makedirs(self.storage_dir)
 
-    def set(self, **params):
-        pass
+    def set(self, key, value):
+        path = os.path.join(self.storage_dir, key)
+        print("path is ", path)
+        output_file = open(path, "w")
+        json.dump(value, output_file)
+        output_file.close()
+
+    def get(self, key):
+        path = os.path.join(self.storage_dir, key)
+        output_file = open(path, "r")
+        value = json.load(output_file)
+        output_file.close()
+
+        return value
+
+    def delete(self, key):
+        path = os.path.join(self.storage_dir, key)
+        os.remove(path)
 
 
 class StorageBackend:
-    def __int__(self):
-        self.mode:bool = False
+    def __init__(self):
+        print("initializing storage backend")
         self.backend = None
-
-        if os.environ.get("storage") == "local":
+        self.storage_type = os.environ.get("SWOBUP_DATASTORAGE", "s3")
+        if self.storage_type == "local":
+            print("using local storage")
             self.backend = LocalStorage()
-            self.mode = False
         else:
+            print("using s3 storage")
             self.backend = S3Backend(app=app)
-            self.mode = True
 
-    def store(self, task_id, data):
-        if self.mode:
-            self.backend.set(key=task_id, value=json.dumps(data))
-        else:
-            self.backend.set()
+    def set(self, key, value):
+        self.backend.set(key=key, value=json.dumps(value))
 
-    def get(self, task_id):
-        data = self.backend.get(task_id)
+
+    def get(self, key):
+        print("getting key")
+        data = self.backend.get(key)
+
+        data = json.loads(data)
+
         return data
+
 
     def delete(self, task_id):
         self.backend.delete(task_id)
