ac71b624557c6203873e7417fff3c358d97f62ca
marcel <marcel.tschoepe@rz.uni-freiburg.de>
2024-07-26 12:14:32 +0200

    bugfix: renamed database_queue

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ echo "starting swobup..."
 
 echo "starting celery"
 celery -A tasks worker -l info &
-celery -A tasks worker --queues=database_queue --concurrency=1 &
+celery -A tasks worker --queues=database_queue --concurrency=1 -l info &
 celery -A tasks flower -l info --port=1111 &
 echo "starting uvicorn"
 uvicorn --workers 10 --host 0.0.0.0 --port 8000 main:app
